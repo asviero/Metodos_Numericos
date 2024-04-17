daaf1836b279cea51f34c717a6a8bed91a1daa59
Angelo Viero <angelosviero@gmail.com>
2024-04-17 16:34:43 -0300

    Alteração no código da Falsa Posição

--- Método da Falsa Posição - Angelo Viero.py
+++ Método da Falsa Posição - Angelo Viero.py
@@ -1,21 +1,17 @@
 import math
 
-'''
-a = float(input("Selecione o ponto a: "))
-b = float(input("Selecione o ponto b: "))
-tol = float(input("Selecione a tolerância: "))
-max_iter = float(input("Selecione a máxima iteração: "))
-'''
-
-def false_position(funcao, a, b, tol, max_iter):
-
+def falsa_posicao(funcao, a, b, tol, max_iter):
+    tabela = []  #Lista para armazenar os valores de x e f(x)
     for i in range(max_iter):
         #Método da Falsa Posição
         x = (a * funcao(b) - b * funcao(a)) / (funcao(b) - funcao(a))
         
+        #Adiciona os valores de x e f(x) na tabela
+        tabela.append((x, funcao(x)))
+        
         #Critério de parada
         if abs(funcao(x)) < tol:
-            return x, funcao(x)
+            return tabela
         
         #Atualiza o intervalo [a, b]
         if funcao(a) * funcao(x) < 0:
@@ -23,11 +19,16 @@ def false_position(funcao, a, b, tol, max_iter):
         else:
             a = x
     
-    raise ValueError("O método não convergiu após o número máximo de iterações.".format(max_iter))
+    raise ValueError("O método não convergiu após o número máximo de iterações.")
 
 def funcao(x):
-    return 2.71828**x + 2**(-x) + 2 * math.cos(x) - 6 #em vez de colocar 'e' elevado na x, coloquei diretamente o valor do número de Euler
+    #return 2.718281**x + 2**(-x) + 2 * math.cos(x) - 6
+    return 4 * math.cos(x) - 2.718281**(2 * x)
 
-estimativa_raiz, funcao_valor = false_position(funcao, a = 1.82, b = (1.84), tol = 0.001, max_iter = 50)
-print("Estimativa da raiz:", estimativa_raiz)
-print("Valor de f(x) = ", funcao_valor)
+tabela = falsa_posicao(funcao, a = 0.5, b = 0.6, tol = 0.001, max_iter = 20)
+
+#Tabela
+print("\tx\tf(x)")
+print("-" * 25)
+for x, fx in tabela:
+    print("{:.6f}\t{:.6f}".format(x, fx))
--- Método de Newton-Raphson - Angelo Viero.py
+++ Método de Newton-Raphson - Angelo Viero.py
@@ -2,7 +2,7 @@ import math
 
 #Método de Newton-Raphson
 def newton_raphson(funcao, derivada, x0, tol, max_iter):
-    tabela = [] #Lista vazia
+    tabela = [] #Lista
     x_antigo = x0
     for i in range(max_iter):
         x_atual = x_antigo - (funcao(x_antigo) / derivada(x_antigo))
@@ -13,13 +13,18 @@ def newton_raphson(funcao, derivada, x0, tol, max_iter):
 
         #Critério de parada 1
         if primeiro_criterio < tol:
-            print("\nRaiz encontrada no primeiro critério de parada.")
+            print("\nRaiz saiu pelo domínio. (Primeiro critério de parada)")
             return x_atual, tabela
         
         #Critério de parada 2
         if segundo_criterio < tol:
-            print("\nRaiz encontrada no segundo critério de parada.")
+            print("\nRaiz saiu pela raiz. (Segundo critério de parada)")
             return x_atual, tabela
+        
+        #Derivada não pode ser próximo de zero
+        if derivada == 0:
+            raise ValueError("Derivada não pode ser zero.")
+            
         
         #Atualiza o valor de x
         x_antigo = x_atual
@@ -27,15 +32,15 @@ def newton_raphson(funcao, derivada, x0, tol, max_iter):
     raise ValueError("Atingiu o número máximo de iterações.")
 
 def funcao(x):
-    return 3 * x - 2.71828 ** (-x) #2.71828 seria o valor de 'e' (Número de Euler)
-    #return 2**x - 3 * x
+    #return 3 * x - 2.71828 ** (-x) #2.71828 seria o valor de 'e' (Número de Euler)
+    return 2**x - 3 * x
 
 def derivada(x):
-    return 3 + 2.71828 ** (-x)
-    #return 2**x * math.log(2) - 3
+    #return 3 + 2.71828 ** (-x)
+    return 2**x * math.log(2) - 3
 
 #Chamada da função
-x0 = 1.5
+x0 = 0.5
 raiz, tabela = newton_raphson(funcao, derivada, x0, tol = 0.0001, max_iter = 10)
 
 
@@ -45,5 +50,5 @@ print("Raiz encontrada:", raiz)
 print("\nTabela de iterações:")
 print("Iteração\t| x\t| Função\t| Derivada\t| Primeiro Critério\t| Segundo Critério")
 print("-" * 100)
-for linha in tabela:
+for linha in tabela[1:]:
     print("\t|".join(map(str, linha)))
