85efcfe892ed856311fad81ed8ce2e733d46c493
Angelo Viero <angelosviero@gmail.com>
2024-04-17 11:50:28 -0300

    Corrigindo a atividade de Newton-Rhapson e apagando alguns arquivos de teste

--- Método da Falsa Posição - Angelo Viero.py
+++ Método da Falsa Posição - Angelo Viero.py
@@ -9,17 +9,13 @@ max_iter = float(input("Selecione a máxima iteração: "))
 
 def false_position(funcao, a, b, tol, max_iter):
 
-    #Condição do método: "a" e "b" precisam ter sinais opostos
-    if funcao(a) * funcao(b) >= 0:
-        raise ValueError("Os valores de f(a) e f(b) têm o mesmo sinal.")
-    
     for i in range(max_iter):
         #Método da Falsa Posição
         x = (a * funcao(b) - b * funcao(a)) / (funcao(b) - funcao(a))
         
         #Critério de parada
         if abs(funcao(x)) < tol:
-            return x #, funcao(x)
+            return x, funcao(x)
         
         #Atualiza o intervalo [a, b]
         if funcao(a) * funcao(x) < 0:
@@ -30,9 +26,8 @@ def false_position(funcao, a, b, tol, max_iter):
     raise ValueError("O método não convergiu após o número máximo de iterações.".format(max_iter))
 
 def funcao(x):
-    return (4 * math.cos(x)) - 2.71828**(2 * x) #em vez de colocar e**(2 * x), coloquei diretamente o valor do número de Euler
+    return 2.71828**x + 2**(-x) + 2 * math.cos(x) - 6 #em vez de colocar 'e' elevado na x, coloquei diretamente o valor do número de Euler
 
-#Caso queira printar o valor de f(x), acrescentar funcao_valor na linha abaixo
-estimativa_raiz = false_position(funcao, a = 0, b = (math.pi / 2), tol = 0.001, max_iter = 50)
+estimativa_raiz, funcao_valor = false_position(funcao, a = 1.82, b = (1.84), tol = 0.001, max_iter = 50)
 print("Estimativa da raiz:", estimativa_raiz)
-#print("Valor de f(x) = ", funcao_valor)
+print("Valor de f(x) = ", funcao_valor)
---
+++ Método de Newton-Raphson - Angelo Viero.py
@@ -0,0 +1,49 @@
+import math
+
+#Método de Newton-Raphson
+def newton_raphson(funcao, derivada, x0, tol, max_iter):
+    tabela = [] #Lista vazia
+    x_antigo = x0
+    for i in range(max_iter):
+        x_atual = x_antigo - (funcao(x_antigo) / derivada(x_antigo))
+        primeiro_criterio = abs(x_atual - x_antigo) / abs(x_atual)
+        segundo_criterio = abs(funcao(x_atual))
+
+        tabela.append([i, x_antigo, funcao(x_antigo), derivada(x_antigo), primeiro_criterio, segundo_criterio])
+
+        #Critério de parada 1
+        if primeiro_criterio < tol:
+            print("\nRaiz encontrada no primeiro critério de parada.")
+            return x_atual, tabela
+        
+        #Critério de parada 2
+        if segundo_criterio < tol:
+            print("\nRaiz encontrada no segundo critério de parada.")
+            return x_atual, tabela
+        
+        #Atualiza o valor de x
+        x_antigo = x_atual
+    
+    raise ValueError("Atingiu o número máximo de iterações.")
+
+def funcao(x):
+    return 3 * x - 2.71828 ** (-x) #2.71828 seria o valor de 'e' (Número de Euler)
+    #return 2**x - 3 * x
+
+def derivada(x):
+    return 3 + 2.71828 ** (-x)
+    #return 2**x * math.log(2) - 3
+
+#Chamada da função
+x0 = 1.5
+raiz, tabela = newton_raphson(funcao, derivada, x0, tol = 0.0001, max_iter = 10)
+
+
+print("Raiz encontrada:", raiz)
+
+#Tabela
+print("\nTabela de iterações:")
+print("Iteração\t| x\t| Função\t| Derivada\t| Primeiro Critério\t| Segundo Critério")
+print("-" * 100)
+for linha in tabela:
+    print("\t|".join(map(str, linha)))
