0c1734c8aa45256352a04feb20f013d4e5af7d88
Angelo Viero <angelosviero@gmail.com>
2024-04-28 23:06:29 -0300

    .

--- Método das Secantes - Angelo Viero.py
+++ Método das Secantes - Angelo Viero.py
@@ -1,6 +1,7 @@
 #Método das Secantes
-def secant_method(f, x0, x1, tol, max_iter):
+def secantes(f, x0, x1, tol, max_iter):
     iter_count = 0
+    
     while iter_count < max_iter:
         #Utilizando o método
         x2 = x1 - (f(x1) * (x1 - x0)) / (f(x1) - f(x0))
@@ -17,19 +18,18 @@ def secant_method(f, x0, x1, tol, max_iter):
     #Se o número máximo de iterações for atingido sem convergência, retorna o valor atual de x
     return x2, max_iter
 
-
 #Função
 def f(x):
     return x**3 - 2*x - 5
 
 #Parâmetros
-x0 = 2  # Primeiro ponto inicial
-x1 = 3  # Segundo ponto inicial
-tolerance = 1e-6  # Tolerância
-max_iterations = 1000  # Número máximo de iterações
+x0 = 2.08
+x1 = 2.1
+tolerance = 0.000001  # Tolerância
+max_iterations = 10  # Número máximo de iterações
 
 # Chamando a função do método das secantes
-root, iterations = secant_method(f, x0, x1, tolerance, max_iterations)
+root, iterations = secantes(f, x0, x1, tolerance, max_iterations)
 
 print("A raiz encontrada é:", root)
 print("Número de iterações realizadas:", iterations)
