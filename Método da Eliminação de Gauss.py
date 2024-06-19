2d16a3a10f9fa6f6c8b4dffca03a8d1ffc3d8176
Angelo <angelosviero@gmail.com>
2024-06-18 21:17:22 -0300

    correção método de gauss

--- Método da Eliminação de Gauss.py
+++ Método da Eliminação de Gauss.py
@@ -2,9 +2,32 @@ import numpy as np
 
 def gauss(A, b):
     n = len(b)
+    #Array de índices para acompanhar as trocas de colunas
+    index = np.arange(n)
 
-    #Eliminação
+    #Pivoteamento completo
     for k in range(n-1):
+        #Encontra o índice do maior valor absoluto na submatriz A[k:n, k:n]
+        max_index = np.unravel_index(np.argmax(abs(A[k:n, k:n])), A[k:n, k:n].shape)
+        max_row = max_index[0] + k
+        max_col = max_index[1] + k
+
+        #Troca a linha k com a linha max_row
+        if max_row != k:
+            A[[k, max_row]] = A[[max_row, k]]
+            b[[k, max_row]] = b[[max_row, k]]
+        
+        #Troca a coluna k com a coluna max_col
+        if max_col != k:
+            A[:, [k, max_col]] = A[:, [max_col, k]]
+            index[[k, max_col]] = index[[max_col, k]]
+        
+        #Verifica se o pivô é zero após a troca
+        if A[k, k] == 0:
+            print("O sistema não tem solução única")
+            return None
+
+        #Eliminação
         for i in range(k+1, n):
             m = A[i][k] / A[k][k]
             A[i][k] = 0
@@ -21,22 +44,28 @@ def gauss(A, b):
             soma += A[i][j] * x[j]
         x[i] = (b[i] - soma) / A[i][i]
 
+    #Reordena a solução de acordo com as trocas de colunas
+    x_temp = np.zeros(n)
+    for i in range(n):
+        x_temp[index[i]] = x[i]
+    x = x_temp
+
     #Arredondamento
     x = np.round(x, decimals=5)
 
     return x
 
 """
-#Exemplo 1
-A = np.array([[2, 2, 1, 1],
-              [1, -1, 2, -1],
-              [3, 2, -3, -2],
-              [4, 3, 2, 1]], dtype = float)
+    #Exemplo 1
+    A = np.array([[2, 2, 1, 1],
+                [1, -1, 2, -1],
+                [3, 2, -3, -2],
+                [4, 3, 2, 1]], dtype = float)
 
-b = np.array([7, 1, 4, 12], dtype = float)
-"""
+    b = np.array([7, 1, 4, 12], dtype = float)
+    """
 
-#Exemplo 3
+# Exemplo 3
 A = np.array([[3, -2, 5, 1],
               [-6, 4, -8, 1],
               [9, -6, 19, 1],
@@ -45,4 +74,5 @@ A = np.array([[3, -2, 5, 1],
 b = np.array([7, -9, 23, 11], dtype = float)
 
 x = gauss(A, b)
-print("Solução: ", x)
+if x is not None:
+    print("Solução: ", x)
